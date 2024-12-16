c37e14c66c1bf2a7279d389bad3e58143fcea86f
Amir <a.abdramanov@aster.kz>
2024-12-16 19:53:35 +0500

    feat: rename css selectors

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="task"
+
     label.innerText=taskString;
     label.className="task__label";
 
